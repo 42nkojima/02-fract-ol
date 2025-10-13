5f4aeab640f13041997a1a624ca3bc293ea814ef
nka21 <naoki.nico@gmail.com>
2025-10-14 00:03:47 +0900

    画像バッファの初期化

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: nkojima <nkojima@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 17:43:00 by nkojima           #+#    #+#             */
-/*   Updated: 2025/10/13 23:40:50 by nkojima          ###   ########.fr       */
+/*   Updated: 2025/10/14 00:03:29 by nkojima          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ typedef struct s_data
 	int max_iter;
 }	t_data;
 
-// キーフックを定義
+// ウィンドウを閉じるキーフックを定義
 int key_hook(int keycode, t_data *data)
 {
 	if (keycode == KEY_ESC)
@@ -54,12 +54,27 @@ int main(void)
 
 	t_data data;
 
+	data.width = 800;
+	data.height = 600;
+	data.min_re = -2.0;
+	data.max_re = 1.0;
+	data.min_im = -1.5;
+	data.max_im = 1.5;
+	data.max_iter = 100;
+
 	data.mlx = mlx_init();
 	if (!data.mlx)
 		return (1);
 
 	data.window = mlx_new_window(data.mlx, 800, 600, "fract-ol");
 	if (!data.window)
+		return (1);
+
+	data.img.img = mlx_new_image(data.mlx, data.width, data.height);
+	if (!data.img.img)
+		return (1);
+	data.img.addr = mlx_get_data_addr(data.img.img, &data.img.bits_per_pixel, &data.img.line_length, &data.img.endian);
+	if (!data.img.addr)
 		return (1);
 
 	mlx_key_hook(data.window, key_hook, &data);
