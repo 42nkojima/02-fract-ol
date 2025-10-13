577edbfec1187f3034ae860f404330eb754b862b
nka21 <naoki.nico@gmail.com>
2025-10-13 18:46:34 +0900

    ESCキーを押すとウィンドウを閉じる

--- include/fractol.h
+++ include/fractol.h
@@ -6,12 +6,14 @@
 /*   By: nkojima <nkojima@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 17:44:00 by nkojima           #+#    #+#             */
-/*   Updated: 2025/10/13 17:47:52 by nkojima          ###   ########.fr       */
+/*   Updated: 2025/10/13 18:32:49 by nkojima          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 #define FRACTOL_H
+
+#define KEY_ESC 65307
 
 #include <mlx.h>
 #include <stdlib.h>
--- src/main.c
+++ src/main.c
@@ -6,26 +6,47 @@
 /*   By: nkojima <nkojima@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 17:43:00 by nkojima           #+#    #+#             */
-/*   Updated: 2025/10/13 17:51:40 by nkojima          ###   ########.fr       */
+/*   Updated: 2025/10/13 18:39:07 by nkojima          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
+typedef struct s_data
+{
+	void *mlx;
+	void *window;
+}	t_data;
+
+// キーフックを定義
+int key_hook(int keycode, t_data *data)
+{
+	if (keycode == KEY_ESC)
+	{
+		mlx_destroy_window(data->mlx, data->window);
+		mlx_destroy_display(data->mlx);
+		free(data->mlx);
+		exit(0);
+	}
+
+	return (0);
+}
+
 int main(void)
 {
 
-	void *mlx;
-	void *window;
+	t_data data;
 
-	mlx = mlx_init();
-	if (!mlx)
+	data.mlx = mlx_init();
+	if (!data.mlx)
 		return (1);
 
-	window = mlx_new_window(mlx, 800, 600, "fract-ol");
-	if (!window)
+	data.window = mlx_new_window(data.mlx, 800, 600, "fract-ol");
+	if (!data.window)
 		return (1);
 
-	mlx_loop(mlx);
+	mlx_key_hook(data.window, key_hook, &data);
+
+	mlx_loop(data.mlx);
 	return (0);
 }
