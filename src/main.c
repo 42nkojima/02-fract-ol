eee01707895c2e4cea5f88c6be0cf6c0b820b46e
nka21 <naoki.nico@gmail.com>
2025-10-15 11:39:23 +0900

    白黒でマンデルブロ集合の描画

--- src/main.c
+++ src/main.c
@@ -6,9 +6,11 @@
 /*   By: nkojima <nkojima@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 17:43:00 by nkojima           #+#    #+#             */
-/*   Updated: 2025/10/14 11:55:32 by nkojima          ###   ########.fr       */
+/*   Updated: 2025/10/15 11:29:12 by nkojima          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// TODO: 全て後でリファクタリングするので、main.cに書く
 
 #include "fractol.h"
 
@@ -28,26 +30,96 @@ typedef struct s_data
 	t_img	img;
 	int		width;
 	int		height;
-	double	min_re;
-	double	max_re;
-	double	min_im;
-	double	max_im;
+	double	min_real;
+	double	max_real;
+	double	min_imag;
+	double	max_imag;
 	int		max_iter;
 }			t_data;
 
 // 指定された座標に色を書き込む
+// * NOTE: 範囲チェックは呼び出し側でする。中でするとパフォーマンス落ちる
 void	put_pixel(t_img *img, int x, int y, int color)
 {
-	char	*dest;
+	int bytes_per_pixel;
+	int offset;
+	char	*pixel;
 
-	dest = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
-	*(unsigned int *)dest = color;
+	bytes_per_pixel = img->bits_per_pixel / 8;
+	offset = y * img->line_length + x * bytes_per_pixel;
+	pixel = img->addr + offset;
+	*(unsigned int *)pixel = color;
 }
 
 // マンデルブロ計算関数
-int mandelbrot(double cr, double ci, int max_iter)
+// * https://ja.wikipedia.org/wiki/%E3%83%9E%E3%83%B3%E3%83%87%E3%83%AB%E3%83%96%E3%83%AD%E9%9B%86%E5%90%88
+int	mandelbrot(double c_real, double c_imag, int max_iter)
 {
-	
+	double z_real = 0.0;
+	double z_imag = 0.0;
+	int iter = 0;
+
+	// x^2 + y^2 <=4 -> まだ発散していない
+	// iter < max_iter -> まだ最大反復回数に達していない
+	while (z_real * z_real + z_imag * z_imag <= 4.0 && iter < max_iter)
+	{
+		double tmp_real = z_real * z_real - z_imag * z_imag + c_real;
+		z_imag = 2.0 * z_real * z_imag + c_imag;
+		z_real = tmp_real;
+		iter++;
+	}
+	return (iter);
+}
+
+// 座標変換関数 (ピクセル -> 複素数)
+// * 座標から褎素数の実部を計算する
+double pixel_to_real(int x, t_data *data)
+{
+	return (data->min_real + (double)x / data->width * (data->max_real - data->min_real));
+}
+
+// * 座標から褎素数の虚部を計算する
+double pixel_to_imag(int y, t_data *data)
+{
+	return (data->min_imag + (double)y / data->height * (data->max_imag - data->min_imag));
+}
+
+// * 反復回数から色を決定
+// TODO: 後で色を変える
+int get_color(int iter, int max_iter)
+{
+	if (iter == max_iter)
+		return (0x00000000);
+	else
+		return (0x00FFFFFF);
+}
+
+// * マンデルブロ集合を描画
+void draw_mandelbrot(t_data *data)
+{
+	int x;
+	int y;
+	double c_real;
+	double c_imag;
+	int iter;
+	int color;
+
+	y = 0;
+	while (y < data->height)
+	{
+		x = 0;
+		while (x < data->width)
+		{
+			c_real = pixel_to_real(x, data);
+			c_imag = pixel_to_imag(y, data);
+			iter = mandelbrot(c_real, c_imag, data->max_iter);
+			color = get_color(iter, data->max_iter);
+			put_pixel(&data->img, x, y, color);
+			x++;
+		}
+		y++;
+	}
+	mlx_put_image_to_window(data->mlx, data->window, data->img.img, 0, 0);
 }
 
 // ウィンドウを閉じるキーフックを定義
@@ -69,10 +141,10 @@ int	main(void)
 
 	data.width = 800;
 	data.height = 600;
-	data.min_re = -2.0;
-	data.max_re = 1.0;
-	data.min_im = -1.5;
-	data.max_im = 1.5;
+	data.min_real = -2.0;
+	data.max_real = 1.0;
+	data.min_imag = -1.5;
+	data.max_imag = 1.5;
 	data.max_iter = 100;
 	data.mlx = mlx_init();
 	if (!data.mlx)
@@ -88,6 +160,7 @@ int	main(void)
 	if (!data.img.addr)
 		return (1);
 	mlx_key_hook(data.window, key_hook, &data);
+	draw_mandelbrot(&data);
 	mlx_loop(data.mlx);
 	return (0);
 }
